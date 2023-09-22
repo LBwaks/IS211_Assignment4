f848ad4d4227b3c162d5addc5ed9a908ff80e807
LBwaks <obwakuvictor@gmail.com>
2023-09-22 23:01:27 +0300

    sortin

--- called sort_compare.py
+++ called sort_compare.py
@@ -1,4 +1,5 @@
 import time
+import random
 
 
 def insertion_sort(alist):
@@ -38,9 +39,43 @@ def gapInsertionSort(alist, start, gap):
         alist[position] = currentValue
 
 
-def python_sort():
-    pass
+def python_sort(alist):
+    starttime = time.time()
+    alist.sort()
+    endtime = time.time()
+    execution_time = endtime - starttime
+    return execution_time
 
 
 def main():
-    pass
+    sizes = [500, 1000, 10000]
+    for size in sizes:
+        num_iterations = 10  # Number of times to run each sorting algorithm
+        insertion_total_time = 0
+        shell_total_time = 0
+        python_total_time = 0
+
+        for _ in range(num_iterations):
+            random_list = [random.randint(1, 1000) for _ in range(size)]
+            insertion_total_time += insertion_sort(random_list.copy())
+            shell_total_time += shell_sort(random_list.copy())
+            python_total_time += python_sort(random_list.copy())
+        average_insertion_time = insertion_total_time / num_iterations
+        average_shell_time = shell_total_time / num_iterations
+        average_python_time = python_total_time / num_iterations
+
+        print(f"List Size: {size}")
+        print(
+            f"Insertion Sort Time took: {average_insertion_time:.6f} seconds to run on average"
+        )
+        print(
+            f"Shell Sort Time took: {average_shell_time:.6f} seconds to run on average"
+        )
+        print(
+            f"Python Sort Time took: {average_python_time:.6f} seconds to run on average"
+        )
+        print()
+
+
+if __name__ == "__main__":
+    main()
